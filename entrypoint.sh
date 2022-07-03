9420484bdc6c78414aea3a94a68fff4fcadcde84
Jorge Maroto <jmaroto@gmail.com>
2022-07-03 06:03:03 +0200

    Fix input name
    To avoid bash trolling

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ sleep 1
 tail -f /tmp/hugo.output | timeout 120 grep -qe 'Press Ctrl+C to stop'
 echo "Hugo has started"
 
-SKIP_FILE=${INPUT_SKIP-FILE}
+SKIP_FILE=${INPUT_SKIPFILE}
 SKIP_FILE_OPTION=""
 test -f "$SKIP_FILE" && SKIP_FILE_OPTION="--skip-file ${GITHUB_WORKSPACE}/${SKIP_FILE}"
 
